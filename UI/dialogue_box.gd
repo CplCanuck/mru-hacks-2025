4bdbf13a8379a1a28b8a08af6aa50dda5689804c
Alpaca404 <32599763+Daniel-M-C@users.noreply.github.com>
2025-10-05 10:59:05 -0600

    BROKEN extra chain links

--- UI/dialogue_box.gd
+++ UI/dialogue_box.gd
@@ -6,38 +6,38 @@ extends Control
 
 
 func _ready():
-    # TODO : forget how to connect to signals
-    for signpost : Signpost in get_tree().get_nodes_in_group("Signposts"):
-        signpost.change_dialogue_box.connect(change_text)
-        signpost.dialogue_box = text_box
-        signpost.clear_text.connect(clear_text)
-    pass
-     
+	# TODO : forget how to connect to signals
+	for signpost : Signpost in get_tree().get_nodes_in_group("Signposts"):
+		signpost.change_dialogue_box.connect(change_text)
+		signpost.dialogue_box = text_box
+		signpost.clear_text.connect(clear_text)
+	pass
+	 
 
 func change_text(text:String, time:int, centered:bool) :
-    # hides on double interact
-    # also stops empty signs from showing anything.
-    if text == text_box.text :
-        visible = false
-        text_box.text = ""
-        text_box.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
-    else :
-        visible = true
-        text_box.text = text
-        if centered :
-            text_box.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-        timer.start(time)
+	# hides on double interact
+	# also stops empty signs from showing anything.
+	if text == text_box.text :
+		visible = false
+		text_box.text = ""
+		text_box.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+	else :
+		visible = true
+		text_box.text = text
+		if centered :
+			text_box.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		timer.start(time)
 
 func clear_text(text):
-    if text == text_box.text :
-        visible = false
-        text_box.text = ""
-        text_box.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+	if text == text_box.text :
+		visible = false
+		text_box.text = ""
+		text_box.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
 
 
 func _on_timer_timeout() -> void:
-    # bad practice
-    visible = false
-    text_box.text = ""
-    
-    pass # Replace with function body.
+	# bad practice
+	visible = false
+	text_box.text = ""
+	
+	pass # Replace with function body.
