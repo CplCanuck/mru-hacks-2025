903e40c8931ce2f9f11608582502a5763e2795b7
CplCanuck <blueguylastname@gmail.com>
2025-10-04 21:11:46 -0600

    Grass

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -11,43 +11,43 @@ var dashing := false
 @onready var interaction_range: Area2D = $InteractionRange
 
 func _physics_process(_delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("left", "right", "up", "down").normalized()
-	if direction:
-		velocity = direction * SPEED
+    # Get the input direction and handle the movement/deceleration.
+    # As good practice, you should replace UI actions with custom gameplay actions.
+    var direction := Input.get_vector("left", "right", "up", "down").normalized()
+    if direction:
+        velocity = direction * SPEED
 
-		# move interaction range node
-		match direction : 
-			Vector2.LEFT:
-				interaction_range.rotation_degrees = 90
-			Vector2.RIGHT:
-				interaction_range.rotation_degrees = 270
-			Vector2.UP:
-				interaction_range.rotation_degrees = 180
-			Vector2.DOWN:
-				interaction_range.rotation_degrees = 0
-	else:
-		# Slow down player if not moving
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-	# Attacking
-	if Input.is_action_pressed("attack"): # Change to is_action_just_pressed for implimented attack
-		attacking = true
-	else:
-		attacking = false
-			
-	# Dash
-	if Input.is_action_just_pressed("dash"):
-		pass
+        # move interaction range node
+        match direction : 
+            Vector2.LEFT:
+                interaction_range.rotation_degrees = 90
+            Vector2.RIGHT:
+                interaction_range.rotation_degrees = 270
+            Vector2.UP:
+                interaction_range.rotation_degrees = 180
+            Vector2.DOWN:
+                interaction_range.rotation_degrees = 0
+    else:
+        # Slow down player if not moving
+        velocity.x = move_toward(velocity.x, 0, SPEED)
+        velocity.y = move_toward(velocity.y, 0, SPEED)
+        
+    # Attacking
+    if Input.is_action_pressed("attack"): # Change to is_action_just_pressed for implimented attack
+        attacking = true
+    else:
+        attacking = false
+            
+    # Dash
+    if Input.is_action_just_pressed("dash"):
+        pass
 
-	move_and_slide()
+    move_and_slide()
 
-	
+    
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed('ui_select'):
-		for object in interaction_range.get_overlapping_areas():
-			if object.has_method("interact"):
-				object.interact()
-		
+    if event.is_action_pressed('ui_select'):
+        for object in interaction_range.get_overlapping_areas():
+            if object.has_method("interact"):
+                object.interact()
+        
--- Scenes/player_animation_tree.gd
+++ Scenes/player_animation_tree.gd
@@ -8,13 +8,13 @@ var last_facing_direction := Vector2(0, -1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	
-	var idle = !player.velocity
-	
-	if !idle:
-		last_facing_direction = player.velocity.normalized()
-	
-	animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
-	animation_tree.set("parameters/Run/blend_position", last_facing_direction)
-	animation_tree.set("parameters/Attack/blend_position", last_facing_direction)
-	animation_tree.set("parameters/Dash/blend_position", last_facing_direction)
+    
+    var idle = !player.velocity
+    
+    if !idle:
+        last_facing_direction = player.velocity.normalized()
+    
+    animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
+    animation_tree.set("parameters/Run/blend_position", last_facing_direction)
+    animation_tree.set("parameters/Attack/blend_position", last_facing_direction)
+    animation_tree.set("parameters/Dash/blend_position", last_facing_direction)
