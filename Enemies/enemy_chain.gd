a401ed403c068df45b5ada60ace68c5cbe8e0c24
Alpaca404 <32599763+Daniel-M-C@users.noreply.github.com>
2025-10-05 03:21:11 -0600

    Add enemy chain descriptions

--- Enemies/enemy_chain.gd
+++ Enemies/enemy_chain.gd
@@ -20,11 +20,35 @@ var can_be_destroyed = false
 
 @onready var chain_enemy_connection: Area2D = $ChainEnemyConnection
 
+enum items {
+	LOCKET,
+	DOLL,
+	WOODEN_HORSE,
+	NECKLACE
+}
+
+var items_dict = {
+	items.LOCKET : "locket" ,#.instantiate(),
+	items.DOLL : "doll" ,#.instantiate(),
+	items.WOODEN_HORSE : "wooden horse",#.instantiate(),
+	items.NECKLACE : "necklace" #.instantiate(),
+}
+
+@export var item : items = items.LOCKET
+@onready var signpost_component: Signpost = $Twinkle/SignpostComponent
+
 
 func _ready() -> void:
 	$chain/PinJoint2D10.set_node_b(enemy.get_path())
 	hurt_box = enemy.get_node("HurtboxComponent")
 	hurt_box.connect("health_depleted",health_depleted)
+	
+	# triggers set
+	signpost_component.text = "\nYou find a + " \
+		+  items_dict[item] \
+		+  "on the ground. \n" \
+		+  "It was very to them to have it tie their ghost down."
+	signpost_component.centered = true
 	
 func health_depleted():
 	$chain.set_modulate(Color(255,0,0,0.5))
@@ -35,6 +59,8 @@ func destroyed():
 	enemy.queue_free()
 	$chain.queue_free()
 	$ChainEnemyConnection.queue_free()
+	signpost_component.monitorable = true
+	signpost_component.monitoring = true
 	
 func _physics_process(delta: float) -> void:
 	if enemy:
