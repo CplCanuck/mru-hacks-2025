e3d6b47f2f27f72f7e1264ef8eeed8948d6ee6fb
CplCanuck <blueguylastname@gmail.com>
2025-10-05 10:24:30 -0600

    Fix Level Boxes

--- Enemies/enemy_chain.gd
+++ Enemies/enemy_chain.gd
@@ -21,61 +21,61 @@ var can_be_destroyed = false
 @onready var chain_enemy_connection: Area2D = $ChainEnemyConnection
 
 enum items {
-	LOCKET,
-	DOLL,
-	WOODEN_HORSE,
-	NECKLACE
+    LOCKET,
+    DOLL,
+    WOODEN_HORSE,
+    NECKLACE
 }
 
 var items_dict = {
-	items.LOCKET : "locket" ,#.instantiate(),
-	items.DOLL : "doll" ,#.instantiate(),
-	items.WOODEN_HORSE : "wooden horse",#.instantiate(),
-	items.NECKLACE : "necklace" #.instantiate(),
+    items.LOCKET : "locket" ,#.instantiate(),
+    items.DOLL : "doll" ,#.instantiate(),
+    items.WOODEN_HORSE : "wooden horse",#.instantiate(),
+    items.NECKLACE : "necklace" #.instantiate(),
 }
 
 @export var item : items = items.LOCKET :
-	set(val):
-		item = val
-		
-		if signpost_component: 
-			signpost_component.text = "\nYou find a " \
-				+  items_dict[item] \
-				+  " on the ground. \n" \
-				+  "It was very important to them to have" \
-				+ " tied their ghost down to earth."
-			signpost_component.centered = true
-		
+    set(val):
+        item = val
+        
+        if signpost_component: 
+            signpost_component.text = "\nYou find a " \
+                +  items_dict[item] \
+                +  " on the ground. \n" \
+                +  "It was very important to them to have" \
+                + " tied their ghost down to earth."
+            signpost_component.centered = true
+        
 @onready var signpost_component: Signpost = $Twinkle/SignpostComponent
 
 
 func _ready() -> void:
-	$chain/PinJoint2D10.set_node_b(enemy.get_path())
-	hurt_box = enemy.get_node("HurtboxComponent")
-	hurt_box.connect("health_depleted",health_depleted)
-	
-	# triggers set
-	signpost_component.text = "\nYou find a" \
-		+  items_dict[item] \
-		+  "on the ground. \n" \
-		+  "It was very important to them to have\n" \
-		+ " tied their ghost down to earth."
-	signpost_component.centered = true
+    $chain/PinJoint2D10.set_node_b(enemy.get_path())
+    hurt_box = enemy.get_node("HurtboxComponent")
+    hurt_box.connect("health_depleted",health_depleted)
+    
+    # triggers set
+    signpost_component.text = "\nYou find a" \
+        +  items_dict[item] \
+        +  "on the ground. \n" \
+        +  "It was very important to them to have\n" \
+        + " tied their ghost down to earth."
+    signpost_component.centered = true
 
 func health_depleted():
-	$chain.set_modulate(Color(255,0,0,0.5))
-	can_be_destroyed = true
-	pass
+    $chain.set_modulate(Color(255,0,0,0.5))
+    can_be_destroyed = true
+    pass
 
 func destroyed():
-	enemy.queue_free()
-	$chain.queue_free()
-	$ChainEnemyConnection.queue_free()
-	signpost_component.monitorable = true
-	signpost_component.monitoring = true
-	
+    enemy.queue_free()
+    $chain.queue_free()
+    $ChainEnemyConnection.queue_free()
+    signpost_component.monitorable = true
+    signpost_component.monitoring = true
+    
 func _physics_process(delta: float) -> void:
-	if enemy:
-		if Vector2.ZERO.distance_to(enemy.position) > enemy_max_range :
-			enemy.position = enemy.position.normalized() * enemy_max_range
-			chain_enemy_connection.get_child(0).shape.b = enemy.position
+    if enemy:
+        if Vector2.ZERO.distance_to(enemy.position) > enemy_max_range :
+            enemy.position = enemy.position.normalized() * enemy_max_range
+            chain_enemy_connection.get_child(0).shape.b = enemy.position
--- Levels/game_manager.gd
+++ Levels/game_manager.gd
@@ -2,29 +2,29 @@ extends Node2D
 
 # NOTE : update this on gravestone.gd if you edit this
 enum levels {
-	NONE,
-	INTRO,
-	HUB,
-	ONE,
-	TWO
+    NONE,
+    INTRO,
+    HUB,
+    ONE,
+    TWO
 }
 var Graves_saved = 0
 
 var level_scenes = {
-	levels.INTRO : preload("res://Levels/intro_level.tscn"),#.instantiate(),
-	levels.HUB : preload("res://Levels/level_graveyard.tscn"),#.instantiate(),
-	levels.ONE : preload("res://Levels/level_one.tscn"),#.instantiate(),
-	levels.TWO : preload("res://Levels/level_two.tscn")#.instantiate(),
+    levels.INTRO : preload("res://Levels/intro_level.tscn"),#.instantiate(),
+    levels.HUB : preload("res://Levels/level_graveyard.tscn"),#.instantiate(),
+    levels.ONE : preload("res://Levels/level_one.tscn"),#.instantiate(),
+    levels.TWO : preload("res://Levels/level_two.tscn")#.instantiate(),
 }
 
 func change_level(level : levels):
-	if level != levels.NONE:
-		#get_window().mode = get_window().MODE_WINDOWED
-		#await get_tree().process_frame
-		print(get_tree().change_scene_to_packed(level_scenes[level]))
-		#await get_tree().process_frame
-		#get_window().mode = get_window().MODE_MAXIMIZED
+    if level != levels.NONE:
+        #get_window().mode = get_window().MODE_WINDOWED
+        #await get_tree().process_frame
+        print(get_tree().change_scene_to_packed(level_scenes[level]))
+        #await get_tree().process_frame
+        #get_window().mode = get_window().MODE_MAXIMIZED
 
 func _ready() :
-	#get_window().mode = get_window().MODE_MAXIMIZED
-	pass
+    #get_window().mode = get_window().MODE_MAXIMIZED
+    pass
