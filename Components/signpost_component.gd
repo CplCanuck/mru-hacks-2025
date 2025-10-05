9200e30eff3af6a4131985dc7f563742e49b8851
CplCanuck <blueguylastname@gmail.com>
2025-10-05 08:54:46 -0600

    Update Intro Text Font

--- Components/signpost_component.gd
+++ Components/signpost_component.gd
@@ -13,20 +13,20 @@ signal clear_text(text)
 var curr_showing : bool = false
 
 func interact():
-	
-	if get_parent() is Gravestone :
-		if dialogue_box and dialogue_box.text == text:
-			GameManager.change_level(get_parent().level)
-	#change_dialogue_box.emit(text, time, centered)
-	if get_parent() is TP_NPC:
-		if dialogue_box and dialogue_box.text == text:
-			GameManager.Graves_saved += 1
-			GameManager.change_level(get_parent().level)
-	change_dialogue_box.emit(text, time, centered)
-	
+    
+    if get_parent() is Gravestone :
+        if dialogue_box and dialogue_box.text == text:
+            GameManager.change_level(get_parent().level)
+    #change_dialogue_box.emit(text, time, centered)
+    if get_parent() is TP_NPC:
+        if dialogue_box and dialogue_box.text == text:
+            GameManager.Graves_saved += 1
+            GameManager.change_level(get_parent().level)
+    change_dialogue_box.emit(text, time, centered)
+    
 
 
 ## Clears text when the player leaves the nearby area
 func _on_ignore_range_body_exited(body: Node2D) -> void:
-	if body is Player:
-		clear_text.emit(text)
+    if body is Player:
+        clear_text.emit(text)
