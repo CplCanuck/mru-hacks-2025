33d6e117e5ff8ee640ef92ba2ae6d8dfbe2dea4d
Anooneee <72642342+Anooneee@users.noreply.github.com>
2025-10-05 08:59:17 -0600

    hj

--- NPCs/senior_npc_tp.gd
+++ NPCs/senior_npc_tp.gd
@@ -19,15 +19,11 @@ func switch_state(state):
 			text = text
 			signpost_component.centered = true
 			pass
-		"Mean":
-			$AnimatedSprite2D.play("idle")
-			$SignpostComponent.process_mode = Node.PROCESS_MODE_INHERIT
 
 func _ready():
 	setup = true
 
 func _on_player_detector_body_entered(body):
-	print("detexted player")
 	if body is Player and setup:
 		switch_state("Scared")
 		print("switching state")
